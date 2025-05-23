6a663682647f84adb7f1531f88dfc74d5599515f
Rijal Solehuddin <solehuddinrizal9@gmail.com>
2025-05-23 10:35:36 +0700

    Perbaikan Seeder sedikit

--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -15,11 +15,12 @@ class TodoSeeder extends Seeder
         $priorities = ['low', 'medium', 'high'];
 
         foreach (range(1, 20) as $i) {
+             $status = $statuses[array_rand($statuses)];
             Todo::create([
                 'title' => 'Todo Item ' . $i,
                 'assignee' => $assignees[array_rand($assignees)],
                 'due_date' => Carbon::now()->addDays(rand(-5, 10))->format('Y-m-d'),
-                'time_tracked' => rand(1, 20) * 0.5, 
+                'time_tracked' => $status === 'completed' ? rand(1, 20) * 0.5 : 0,
                 'status' => $statuses[array_rand($statuses)],
                 'priority' => $priorities[array_rand($priorities)],
                 'created_at' => now(),
